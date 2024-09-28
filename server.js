a00bf944b41dfdfcff1f77ac04bef5c4079aac2c
Lavanya2109-B8 <lavanyachowdhary21@gmail.com>
2024-09-28 12:56:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,4 @@
+console.log("welcome to express");
+console.log("it is my second console screen");
+console.log("it is my third scren");
+console.log("adding again");
--- test.js
+++ test.js
@@ -1,4 +1,19 @@
-for(let a=0;a<5;a++){
-    console.log("inside block of a="+a);
-}
-console.log("outside block a value is="+a);
+const express=require("express")
+const MongoClient=require("mongoose");
+const dotEnv=require("dotenv");
+const app=express();
+
+dotEnv.config();
+const PORT=process.env.PORT || 5678;
+
+MongoClient.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB Atlas Cloud connected successfully")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.listen(PORT,()=>{
+    console.log(`My port is running on ${PORT} number`)
+})
